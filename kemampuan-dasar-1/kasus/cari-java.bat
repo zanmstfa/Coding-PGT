e2f43d2df351202442a7101d49fa4bfa49a7fe2e
Fauzan Mustofa <70005931+zanmstfa@users.noreply.github.com>
2022-08-15 08:08:35 +0700

    Kemampuan Dasar

--- kemampuan-dasar-1/kasus/cari-java.bat
+++ kemampuan-dasar-1/kasus/cari-java.bat
@@ -1,1 +1,11 @@
+@echo off
 
+cd C:\Users\ojank\Coding-PGT\Kemampuan Dasar\kemampuan-dasar-1\kasus
+
+echo Ada file java pada direktori: C:\Users\ojank\Coding-PGT\Kemampuan Dasar\kemampuan-dasar-1\kasus
+
+dir C:\Users\ojank\Coding-PGT\Kemampuan Dasar\kemampuan-dasar-1\kasus
+
+dir /s *.java
+
+PAUSE
